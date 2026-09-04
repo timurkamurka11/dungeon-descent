0e7c2fd7db520763f63c7d2a120f27025fa9d7c5
timurkamurka11 <timur2417@gmail.com>
2026-09-05 00:01:15 +0300

    feat: play rigged death animation on player death

--- Assets/DungeonDescent/Runtime/Player/PlayerLifecycle.cs
+++ Assets/DungeonDescent/Runtime/Player/PlayerLifecycle.cs
@@ -9,10 +9,10 @@ namespace DungeonDescent.Player
     [RequireComponent(typeof(PlayerVitals))]
     public sealed class PlayerLifecycle:MonoBehaviour
     {
-        private PlayerVitals vitals;private PlayerController controller;
-        private void Awake(){vitals=GetComponent<PlayerVitals>();controller=GetComponent<PlayerController>();}
+        private PlayerVitals vitals;private PlayerController controller;private PlayerAnimationController animationController;
+        private void Awake(){vitals=GetComponent<PlayerVitals>();controller=GetComponent<PlayerController>();animationController=GetComponent<PlayerAnimationController>();}
         private void OnEnable(){vitals.Died+=OnDied;}private void OnDisable(){vitals.Died-=OnDied;}
         private void OnDied(){StartCoroutine(DeathRoutine());}
-        private IEnumerator DeathRoutine(){controller?.SetMovementLocked(true);GameUI.Instance?.ShowDeath();yield return new WaitForSecondsRealtime(2.25f);DungeonWorldBuilder.Instance?.ReturnPlayerToSafeRoom(true);GetComponent<PlayerConsumables>()?.Refill();GameUI.Instance?.HideDeath();yield return new WaitForSecondsRealtime(.15f);controller?.SetMovementLocked(false);}
+        private IEnumerator DeathRoutine(){controller?.SetMovementLocked(true);animationController?.PlayDeath();GameUI.Instance?.ShowDeath();yield return new WaitForSecondsRealtime(2.25f);DungeonWorldBuilder.Instance?.ReturnPlayerToSafeRoom(true);GetComponent<PlayerConsumables>()?.Refill();animationController?.ResetPose();GameUI.Instance?.HideDeath();yield return new WaitForSecondsRealtime(.15f);controller?.SetMovementLocked(false);}
     }
 }
